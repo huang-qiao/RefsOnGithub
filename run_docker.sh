d785236d324d6edb60316272095059cbb9d08ee1
qiao <qiao.helloworld@gmail.com>
2018-08-02 00:47:22 +0800

    bugfix: missing /etc and /log for opengrok indexing

--- run_docker.sh
+++ run_docker.sh
@@ -3,10 +3,14 @@
 BASE_DIR="$( cd "$(dirname "$0")" ; pwd -P )"
 OPENGROK_SRC=${BASE_DIR}/opengrok/src
 OPENGROK_DATA=${BASE_DIR}/opengrok/data
+OPENGROK_ETC=${BASE_DIR}/opengrok/etc
+OPENGROK_LOG=${BASE_DIR}/opengrok/log
 
 docker run -td --rm \
   --privileged=true \
   --volume="${OPENGROK_SRC}:/var/opengrok/src" \
   --volume="${OPENGROK_DATA}:/var/opengrok/data" \
+  --volume="${OPENGROK_ETC}:/var/opengrok/etc" \
+  --volume="${OPENGROK_LOG}:/var/opengrok/log" \
   -p 8080:8080 \
   opengrok
