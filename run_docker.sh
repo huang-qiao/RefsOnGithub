fcf3789dea7e359ce6fe76a36156e225b8b89b90
qiao <qiao.helloworld@gmail.com>
2019-09-08 21:00:45 +0800

    update docker image and command

--- run_docker.sh
+++ run_docker.sh
@@ -8,8 +8,10 @@ OPENGROK_ETC=${BASE_DIR}/opengrok/etc
 #docker run -it --rm \
 docker run -tdv --rm \
   --privileged=true \
+  -e REINDEX="60" \
   --volume="${OPENGROK_SRC}:/opengrok/src" \
   --volume="${OPENGROK_DATA}:/opengrok/data" \
-  --volume="${OPENGROK_ETC}:/var/opengrok/etc" \
-  -p 8080:8080 \
+  --volume="${OPENGROK_ETC}:/opengrok/etc" \
+  -p 8080:8080/tcp \
   opengrok/docker:latest
+
